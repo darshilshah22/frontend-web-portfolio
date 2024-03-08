0a12b482d0708810f7993c0a1cb24ca94b3ae84b
darshilshah22 <shah.darshilp@gmail.com>
2024-03-08 23:56:04 +0530

    add opensea

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -3,14 +3,16 @@ export const projectsList = [
     name: "Wearthy",
     type: "Web App",
     description:
-      "A weather app where user can get weather predictions and more.",
+      // "A weather app where user can get weather predictions and more.",
+      "ReactJS, HTML, CSS, JavaScript, Weather APIs",
     image: "/assets/weather.png",
     link: "https://wearthy.netlify.app/",
   },
   {
     name: "Tradesman's Trust",
     type: "Mobile App",
-    description: "It is a secure, trust-based job and task payment platform.",
+    // description: "It is a secure, trust-based job and task payment platform.",
+    description: "Flutter, REST APIs, Firebase Cloud Messaging",
     image: "/assets/tt.png",
     link: "https://play.google.com/store/apps/details?id=com.trademen.www&hl=en-IN",
   },
@@ -18,7 +20,8 @@ export const projectsList = [
     name: "E-Shop",
     type: "Web App",
     description:
-      "Explore top tech trends on our React.js electronics website.",
+      // "Explore top tech trends on our React.js electronics website.",
+      "ReactJS, NodeJS, HTML, CSS, JavaScript, Bootstrap",
     image: "/assets/eshop.png",
     link: "https://eshop-frontend.netlify.app/",
   },
@@ -26,8 +29,17 @@ export const projectsList = [
     name: "Discover",
     type: "Mobile App",
     description:
-      "Your travel companion for seamless journeys and exploration.",
+      // "Your travel companion for seamless journeys and exploration.",
+      "Flutter, Provider, REST APIs, Firebase Messaging",
     image: "/assets/discover.jpeg",
     link: "https://play.google.com/store/apps/details?id=com.snapsales.dscvr&hl=en-IN",
+  },
+  {
+    name: "OpenSea Clone",
+    type: "Web App",
+    description:
+      "ReactJS, HTML, CSS, JavaScript",
+    image: "/assets/opensea.png",
+    link: "https://openseaclone.netlify.app/",
   },
 ];
